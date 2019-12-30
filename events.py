291b81f27033dc9229fcd3f92a2d34afc50c763f
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-30 16:33:26 +0100

    Removing miners and implementing chain rule

--- events.py
+++ events.py
@@ -3,3 +3,4 @@ from collections import namedtuple
 ChamberAdded = namedtuple('ChamberAdded', ['x', 'y'])
 
 MinerPlaced = namedtuple('MinerPlaced', ['x', 'y'])
+MinerRemoved = namedtuple('MinerRemoved', ['x', 'y'])
--- magnum-sal.py
+++ magnum-sal.py
@@ -1,6 +1,6 @@
 import unittest
 
-from events import ChamberAdded, MinerPlaced
+from events import ChamberAdded, MinerPlaced, MinerRemoved
 
 
 class ApplicationException(Exception):
@@ -11,6 +11,7 @@ class MagnumSal:
   def __init__(self, events):
     self.events = events
     self.miner_placement_service = MinerPlacementService(events)
+    self.miner_removal_service = MinerRemovalService(events)
 
   def add_chamber(self, x, y):
     self.events.append(ChamberAdded(x, y))
@@ -40,6 +41,52 @@ class MinerPlacementService:
     self.events.append(MinerPlaced(x, y))
 
 
+class MinerRemovalService:
+  def __init__(self, events):
+    self.events = events
+
+  def remove_miner(self, x, y):
+    # is there a miner at x, y
+    miners_placed_there = [mp for mp in self.events if isinstance(mp, MinerPlaced) and mp.x == x and mp.y == y]
+
+    if not miners_placed_there:
+      raise ApplicationException("There was no miner to be removed at [%s, %s]" % (x, y))
+
+    miners_removed_there = [mp for mp in self.events if isinstance(mp, MinerRemoved) and mp.x == x and mp.y == y]
+
+    # Are there miners left, after considering removals?
+    miners_there = len(miners_placed_there) - len(miners_removed_there)
+    if miners_there == 0:
+      raise ApplicationException("All miners at [%s, %s] were already removed" % (x, y))
+
+    # Do we not break the chain?
+    neighbors_dxdy = [
+      (-1, 0),
+      (1, 0),
+      (0, 1),
+      (0, -1)
+    ]
+
+    chambers_with_neighbors = 0
+    # if there are neighbors on 2 sides, then we're in the middle of a chain
+    for ndx, ndy in neighbors_dxdy:
+      n_miners_placed = [mp for mp in self.events if
+                         isinstance(mp, MinerPlaced) and mp.x == x + ndx and mp.y == y + ndy]
+      n_miners_removed = [mr for mr in self.events if
+                          isinstance(mr, MinerRemoved) and mr.x == x + ndx and mr.y == y + ndy]
+      n_has_neighbor = len(n_miners_placed) - len(n_miners_removed) > 0
+
+      if n_has_neighbor:
+        chambers_with_neighbors += 1
+
+    in_chain = chambers_with_neighbors == 2
+
+    if in_chain and miners_there == 1:
+      raise ApplicationException("You cannot remove a miner from the middle of a chain at [%s, %s]" % (x, y))
+
+    self.events.append(MinerRemoved(x, y))
+
+
 class Tests(unittest.TestCase):
 
   def test_init_world(self):
@@ -49,6 +96,8 @@ class Tests(unittest.TestCase):
     game.add_chamber(0, 0)
 
     self.assertEqual([ChamberAdded(0, 0)], events)
+
+  # Placing miners
 
   def test_place_miner_at_start_of_mine(self):
     events = [
@@ -98,3 +147,130 @@ class Tests(unittest.TestCase):
 
     game.miner_placement_service.place_miner(1, 2)
     self.assertIn(MinerPlaced(1, 2), [e for e in events if isinstance(e, MinerPlaced)])
+
+  # Removing Miners
+
+  def test_we_can_remove_the_first_miner(self):
+    events = [
+      ChamberAdded(0, 0),
+      MinerPlaced(0, 0),
+    ]
+
+    game = MagnumSal(events)
+
+    game.miner_removal_service.remove_miner(0, 0)
+
+    self.assertIn(MinerRemoved(0, 0), [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_cannot_remove_a_non_existant_miner(self):
+    events = [
+      ChamberAdded(0, 0),
+    ]
+
+    game = MagnumSal(events)
+
+    self.assertRaises(ApplicationException,
+                      lambda: game.miner_removal_service.remove_miner(0, 0))
+    self.assertEquals([], [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_cannot_remove_a_previously_removed_miner(self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerRemoved(0, 1),
+    ]
+
+    game = MagnumSal(events)
+
+    self.assertRaises(ApplicationException,
+                      lambda: game.miner_removal_service.remove_miner(0, 1))
+    self.assertEquals([MinerRemoved(0, 1)], [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_can_remove_a_removed_and_readded_miner(self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerRemoved(0, 1),
+      MinerPlaced(0, 1),
+    ]
+
+    game = MagnumSal(events)
+
+    game.miner_removal_service.remove_miner(0, 1)
+
+    self.assertEquals([MinerRemoved(0, 1), MinerRemoved(0, 1)], [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_can_remove_the_last_miner_in_a_chain(self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      ChamberAdded(0, 2),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerPlaced(-1, 1),
+      MinerPlaced(0, 2),
+    ]
+
+    game = MagnumSal(events)
+
+    game.miner_removal_service.remove_miner(-1, 1)
+
+    self.assertIn(MinerRemoved(-1, 1), [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_cannot_remove_a_neighbor_in_the_middle_of_a_chain(self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      ChamberAdded(0, 2),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerPlaced(0, 2),
+    ]
+
+    game = MagnumSal(events)
+
+    self.assertRaises(ApplicationException,
+                      lambda: game.miner_removal_service.remove_miner(0, 1))
+
+    self.assertNotIn(MinerRemoved(0, 1), [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_can_remove_a_neighbor_in_the_middle_of_a_chain_if_the_chain_does_not_break(self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      ChamberAdded(0, 2),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerPlaced(0, 1),
+      MinerPlaced(0, 2),
+    ]
+
+    game = MagnumSal(events)
+
+    game.miner_removal_service.remove_miner(0, 1)
+
+    self.assertEqual([MinerRemoved(0, 1)], [e for e in events if isinstance(e, MinerRemoved)])
+
+  def test_we_cannot_remove_a_neighbor_in_the_middle_of_a_chain_if_the_chain_breaks_with_removals_taken_into_account(
+      self):
+    events = [
+      ChamberAdded(0, 0),
+      ChamberAdded(0, 1),
+      ChamberAdded(0, 2),
+      MinerPlaced(0, 0),
+      MinerPlaced(0, 1),
+      MinerPlaced(0, 1),
+      MinerPlaced(0, 2),
+      MinerRemoved(0, 1),
+    ]
+
+    game = MagnumSal(events)
+
+    self.assertRaises(ApplicationException,
+                      lambda: game.miner_removal_service.remove_miner(0, 1))
+
+    self.assertEqual([MinerRemoved(0, 1)], [e for e in events if isinstance(e, MinerRemoved)])
